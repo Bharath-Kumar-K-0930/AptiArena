250213de2bb77da9cb6e8f8de453d83e43f9b794
Bharath-Kumar-K-0930 <bharathkumarkbk9@gmail.com>
2026-01-31 01:08:46 +0530

    Fix Nodemailer config: remove 'service' shortcut to force port 587

--- server/src/services/emailService.ts
+++ server/src/services/emailService.ts
@@ -4,7 +4,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
     host: 'smtp.gmail.com',
     port: 587,
     secure: false, // true for 465, false for other ports
